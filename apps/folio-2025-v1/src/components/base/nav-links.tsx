5484946c4141caca5ba388f83acc250105b2c1ec
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-10 11:33:24 +0100

    style: Add transition on nav links

--- apps/folio-2025-v1/src/components/base/nav-links.tsx
+++ apps/folio-2025-v1/src/components/base/nav-links.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cn } from '@/lib/utils/cn'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useState } from 'react'
@@ -15,9 +16,24 @@ export function NavLinks(props: { href: string; children: React.ReactNode }) {
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <span className={isActive || hovered ? 'opacity-100' : 'opacity-0'}>( </span>
+      <span
+        className={cn(
+          'transition-all duration-200',
+          isActive || hovered ? 'opacity-100' : 'opacity-0',
+        )}
+      >
+        ({' '}
+      </span>
       {props.children}
-      <span className={isActive || hovered ? 'opacity-100' : 'opacity-0'}> )</span>
+      <span
+        className={cn(
+          'transition-all duration-200',
+          isActive || hovered ? 'opacity-100' : 'opacity-0',
+        )}
+      >
+        {' '}
+        )
+      </span>
     </Link>
   )
 }
