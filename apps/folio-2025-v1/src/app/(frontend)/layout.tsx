5fd77b1a868bbe0d1fcaa18f56d36b53c231a809
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-10 11:34:12 +0100

    style: Add fonts and Smoothscrolling

--- apps/folio-2025-v1/src/app/(frontend)/layout.tsx
+++ apps/folio-2025-v1/src/app/(frontend)/layout.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import './styles.css'
 import { MainLayout } from '@/components/layout/Main'
+import localFont from 'next/font/local'
+import { Geist } from 'next/font/google'
+import SmoothScrolling from '@/components/contexts/smooth-scroll'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
 
+const madeMirage = localFont({
+  src: '../../assets/fonts/made-mirage-thin.otf',
+  variable: '--font-made-mirage',
+})
+
+const geist = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist',
+})
+
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
+    <html lang="en" className={`${madeMirage.variable} ${geist.variable}`}>
       <body>
-        <MainLayout>{children}</MainLayout>
+        <SmoothScrolling>
+          <MainLayout>{children}</MainLayout>
+        </SmoothScrolling>
       </body>
     </html>
   )
---
+++ apps/folio-2025-v1/src/components/contexts/smooth-scroll.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { ReactLenis } from '@studio-freight/react-lenis'
+
+function SmoothScrolling({ children }: { children: any }) {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 2 }}>
+      {children}
+    </ReactLenis>
+  )
+}
+
+export default SmoothScrolling
