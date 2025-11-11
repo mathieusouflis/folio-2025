a8bfa35f51bf43898ca82e34463aa24057a62727
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-11 14:13:38 +0100

    feat: Add project feature components and sections

---
+++ apps/folio-2025-v1/src/features/project/components/launch.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { TP } from '@/components/typograpgy/p'
+import { cn } from '@/lib/utils/cn'
+import Link from 'next/link'
+
+export function LaunchButton({ url, className }: { url: string; className?: string }) {
+  return (
+    <Link href={url} className={cn('group px-1 py-0.5', className)}>
+      <span className="absolute bg-black w-full h-0 left-0 -bottom-px transition-all duration-300 group-hover:h-full"></span>
+      <TP className={cn('text-white mix-blend-difference w-max font-medium')}>Launch Website</TP>
+    </Link>
+  )
+}
