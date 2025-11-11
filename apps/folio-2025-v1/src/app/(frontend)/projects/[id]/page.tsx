c8f9e16ebbddebd1332ca7858057bc98b109b7c6
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-11 14:12:08 +0100

    feat: Load project data and render ProjectPage

--- apps/folio-2025-v1/src/app/(frontend)/projects/[id]/page.tsx
+++ apps/folio-2025-v1/src/app/(frontend)/projects/[id]/page.tsx
@@ -1,7 +1,26 @@
-export default async function ProjectPage() {
-  return (
-    <div>
-      <h1>Project Page</h1>
-    </div>
-  )
+import { ProjectPage } from '@/features/project'
+import { Project } from '@/payload-types'
+import config from '@/payload.config'
+import { redirect } from 'next/navigation'
+import { getPayload } from 'payload'
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const payloadConfig = await config
+  const payload = await getPayload({ config: payloadConfig })
+  const { id } = await params
+
+  let project: Project
+
+  try {
+    project = await payload.findByID({
+      collection: 'projects',
+      id,
+      depth: 1,
+    })
+  } catch (err) {
+    console.error(err)
+    return redirect('/')
+  }
+
+  return <ProjectPage project={project} />
 }
