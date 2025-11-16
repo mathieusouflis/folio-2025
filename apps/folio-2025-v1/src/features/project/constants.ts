6e1d02a51ad272bfc27fc65bff5afbd4b611183e
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-16 13:30:36 +0100

    refactor: Refactor project description and add projectUrl

--- apps/folio-2025-v1/src/features/project/constants.ts
+++ apps/folio-2025-v1/src/features/project/constants.ts
@@ -1,5 +1,19 @@
-export const CATEGORIES: ('clientName' | 'skills' | 'endDate')[] = [
-  'clientName',
-  'skills',
-  'endDate',
+import { Project } from '@/payload-types'
+
+export const CATEGORIES: {
+  key: keyof Project
+  displayName: string
+}[] = [
+  {
+    key: 'clientName',
+    displayName: 'Client Name',
+  },
+  {
+    key: 'skills',
+    displayName: 'Skills',
+  },
+  {
+    key: 'endDate',
+    displayName: 'End Date',
+  },
 ] as const
