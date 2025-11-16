98a311d742a94b732ca3bf00f0361cd64c2fa337
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-16 12:39:27 +0100

    feat: Replace ShowReel with Next Image background
    Delete ShowReel componentl and update header-section to render a
    Next/Image as a full-lbleed background and adjust layout/classes. Remove
    stray /** @type {any} */ comment from importMap.

--- apps/folio-2025-v1/src/features/project/sections/header-section.tsx
+++ apps/folio-2025-v1/src/features/project/sections/header-section.tsx
@@ -1,20 +1,29 @@
 import { SectionFullPage } from '@/components/base/section-full-page'
 import { TDisplay } from '@/components/typograpgy/display'
 import { LaunchButton } from '../components/launch'
-import { ShowReel } from '../components/showreel'
 import { Media } from '@/payload-types'
+import Image from 'next/image'
 
 export function ProjectHeaderSection(params: {
   title: string
   showreel?: Media | undefined | null | number
 }) {
   return (
-    <SectionFullPage className="flex-col items-center justify-center">
-      <TDisplay className="w-1/2 text-center">{params.title}</TDisplay>
-      <LaunchButton url="absolute bottom-0" className="absolute bottom-9" />
-
-      {params.showreel && typeof params.showreel !== 'number' && params.showreel.url && (
-        <ShowReel url={params.showreel.url} className="absolute bottom-12 right-0" />
+    <SectionFullPage className="ml-0 mr-0">
+      <div className="flex flex-col w-full items-center justify-center ml-(--gridMargin) mr-(--gridMargin)">
+        <TDisplay className="w-1/2 text-center mix-blend-difference text-white">
+          {params.title}
+        </TDisplay>
+        <LaunchButton url="absolute bottom-0" className="absolute bottom-9" />
+      </div>
+      {params.showreel && typeof params.showreel === 'object' && (
+        <Image
+          src={params.showreel?.url || ''}
+          alt={params.showreel?.alt || ''}
+          width={params.showreel?.width || 0}
+          height={params.showreel?.height || 0}
+          className="absolute pointer-events-none -z-50 top-0 left-0 w-full h-full object-cover"
+        />
       )}
     </SectionFullPage>
   )
