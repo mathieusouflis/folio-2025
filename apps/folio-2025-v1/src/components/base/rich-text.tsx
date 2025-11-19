98c20ad43375d668292054c9a18361e87323f4c1
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-19 13:53:19 +0100

    feat: Add class support for custom components

--- apps/folio-2025-v1/src/components/base/rich-text.tsx
+++ apps/folio-2025-v1/src/components/base/rich-text.tsx
@@ -1,4 +1,4 @@
-import type { DefaultNodeTypes, SerializedLinkNode } from '@payloadcms/richtext-lexical'
+import type { DefaultNodeTypes } from '@payloadcms/richtext-lexical'
 import type {
   SerializedEditorState,
   SerializedLexicalNode,
@@ -6,7 +6,6 @@ import type {
 
 import {
   JSXConvertersFunction,
-  LinkJSXConverter,
   RichText as RichTextConverter,
 } from '@payloadcms/richtext-lexical/react'
 import React from 'react'
@@ -18,54 +17,39 @@ import { TH4 } from '../typograpgy/h4'
 import { TList, TListItem } from '../typograpgy/list'
 import { TBlockquote } from '../typograpgy/blockquote'
 
-const internalDocToHref = ({ linkNode }: { linkNode: SerializedLinkNode }) => {
-  const { relationTo, value } = linkNode.fields.doc!
-  if (typeof value !== 'object') {
-    throw new Error('Expected value to be an object')
-  }
-  const slug = value.slug
-
-  switch (relationTo) {
-    case 'posts':
-      return `/posts/${slug}`
-    case 'categories':
-      return `/category/${slug}`
-    case 'pages':
-      return `/${slug}`
-    default:
-      return `/${relationTo}/${slug}`
-  }
-}
-
-const jsxConverters: JSXConvertersFunction<DefaultNodeTypes> = ({ defaultConverters }) => ({
-  ...defaultConverters,
-  paragraph: ({ nodesToJSX, node, ...args }) => (
-    <TP>{nodesToJSX({ nodes: node.children, ...args })}</TP>
-  ),
-  heading: ({ nodesToJSX, node, ...args }) =>
-    node.tag === 'h1' ? (
-      <TH1>{nodesToJSX({ nodes: node.children, ...args })}</TH1>
-    ) : node.tag === 'h2' ? (
-      <TH2>{nodesToJSX({ nodes: node.children, ...args })}</TH2>
-    ) : node.tag === 'h3' ? (
-      <TH3>{nodesToJSX({ nodes: node.children, ...args })}</TH3>
-    ) : node.tag === 'h4' ? (
-      <TH4>{nodesToJSX({ nodes: node.children, ...args })}</TH4>
-    ) : null,
-  list: ({ nodesToJSX, node, ...args }) => (
-    <TList>{nodesToJSX({ nodes: node.children, ...args })}</TList>
-  ),
-  listitem: ({ nodesToJSX, node, ...args }) => (
-    <TListItem>{nodesToJSX({ nodes: node.children, ...args })}</TListItem>
-  ),
-  quote: ({ nodesToJSX, node, ...args }) => (
-    <TBlockquote>{nodesToJSX({ nodes: node.children, ...args })}</TBlockquote>
-  ),
-  ...LinkJSXConverter({ internalDocToHref }),
-})
+const jsxConverters =
+  (className?: string): JSXConvertersFunction<DefaultNodeTypes> =>
+  ({ defaultConverters }) => ({
+    ...defaultConverters,
+    paragraph: ({ nodesToJSX, node, ...args }) => (
+      <TP className={className}>{nodesToJSX({ nodes: node.children, ...args })}</TP>
+    ),
+    heading: ({ nodesToJSX, node, ...args }) =>
+      node.tag === 'h1' ? (
+        <TH1 className={className}>{nodesToJSX({ nodes: node.children, ...args })}</TH1>
+      ) : node.tag === 'h2' ? (
+        <TH2 className={className}>{nodesToJSX({ nodes: node.children, ...args })}</TH2>
+      ) : node.tag === 'h3' ? (
+        <TH3 className={className}>{nodesToJSX({ nodes: node.children, ...args })}</TH3>
+      ) : node.tag === 'h4' ? (
+        <TH4 className={className}>{nodesToJSX({ nodes: node.children, ...args })}</TH4>
+      ) : null,
+    list: ({ nodesToJSX, node, ...args }) => (
+      <TList className={className}>{nodesToJSX({ nodes: node.children, ...args })}</TList>
+    ),
+    listitem: ({ nodesToJSX, node, ...args }) => (
+      <TListItem className={className}>{nodesToJSX({ nodes: node.children, ...args })}</TListItem>
+    ),
+    quote: ({ nodesToJSX, node, ...args }) => (
+      <TBlockquote className={className}>
+        {nodesToJSX({ nodes: node.children, ...args })}
+      </TBlockquote>
+    ),
+  })
 
 export const RichText: React.FC<{
   lexicalData: SerializedEditorState<SerializedLexicalNode>
-}> = ({ lexicalData }) => {
-  return <RichTextConverter converters={jsxConverters} data={lexicalData} />
+  className?: string
+}> = ({ lexicalData, className }) => {
+  return <RichTextConverter converters={jsxConverters(className)} data={lexicalData} />
 }
