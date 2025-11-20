f3df03e3e8de8cf688c079ecb83ad5426430a382
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-20 14:04:26 +0100

    refactor: Replace SectionFullPage with SectionGridFullPage
    Use Grid and GridItem for layout inside the full-page section and update
    all project sections to import and use the new component. Also update
    the about page to export Page that renders AboutPage.

--- apps/folio-2025-v1/src/features/project/sections/collaborators/index.tsx
+++ apps/folio-2025-v1/src/features/project/sections/collaborators/index.tsx
@@ -1,4 +1,4 @@
-import { SectionFullPage } from '@/components/base/section-full-page'
+import { SectionGridFullPage } from '@/components/base/section-full-page'
 import { Project } from '@/payload-types'
 import { CollaboratorCard } from './collaborator-card'
 import { TH2 } from '@/components/typograpgy/h2'
@@ -7,7 +7,7 @@ export function ProjectCollaboratorsSection(props: {
   collaborators: Project['collaborators'] | null | undefined
 }) {
   return (
-    <SectionFullPage className="py-24 gap-3 flex-col justify-center items-center gap-24">
+    <SectionGridFullPage className="py-24 gap-3 flex-col justify-center items-center gap-24">
       <TH2 className="text-2xl uppercase">Crédits</TH2>
       <div className="gap-2 flex flex-col">
         {props.collaborators &&
@@ -32,6 +32,6 @@ export function ProjectCollaboratorsSection(props: {
             )
           })}
       </div>
-    </SectionFullPage>
+    </SectionGridFullPage>
   )
 }
