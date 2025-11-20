b8d36d6eda702eea0e32673a275752e01e282b07
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-20 14:31:47 +0100

    refactor(grid-setup): Fix project-header component

--- apps/folio-2025-v1/src/features/project/sections/header-section.tsx
+++ apps/folio-2025-v1/src/features/project/sections/header-section.tsx
@@ -12,36 +12,34 @@ export function ProjectHeaderSection(params: {
 }) {
   return (
     <SectionGridFullPage className="ml-0 mr-0">
-      <GridItem span="full">
-        <div className="relative flex flex-col w-full h-full items-center justify-center">
-          <TDisplay className="w-1/2 text-center mix-blend-difference text-white">
-            {params.title}
-          </TDisplay>
-          <Grid withMargins={false} className="w-full absolute bottom-1.5 right-0">
-            <GridItem
-              start={10}
-              end={13}
-              as={TP}
-              className="text-white flex justify-between  mix-blend-difference uppercase"
-            >
-              (
-              {new Array(4).fill('Scroll').map((scrollText, idx) => {
-                return <span key={idx}>{scrollText}</span>
-              })}
-              )
-            </GridItem>
-          </Grid>
-        </div>
-        {params.showreel && typeof params.showreel === 'object' && (
-          <Image
-            src={params.showreel?.url || ''}
-            alt={params.showreel?.alt || ''}
-            width={params.showreel?.width || 0}
-            height={params.showreel?.height || 0}
-            className="absolute pointer-events-none -z-50 top-0 left-0 w-full h-full object-cover brightness-65"
-          />
-        )}
-      </GridItem>
+      <div className="relative flex flex-col w-full h-full items-center justify-center">
+        <TDisplay className="w-1/2 text-center mix-blend-difference text-white">
+          {params.title}
+        </TDisplay>
+        <Grid withMargins={false} className="w-full absolute bottom-1.5 right-0">
+          <GridItem
+            start={10}
+            end={13}
+            as={TP}
+            className="text-white flex justify-between  mix-blend-difference uppercase"
+          >
+            (
+            {new Array(4).fill('Scroll').map((scrollText, idx) => {
+              return <span key={idx}>{scrollText}</span>
+            })}
+            )
+          </GridItem>
+        </Grid>
+      </div>
+      {params.showreel && typeof params.showreel === 'object' && (
+        <Image
+          src={params.showreel?.url || ''}
+          alt={params.showreel?.alt || ''}
+          width={params.showreel?.width || 0}
+          height={params.showreel?.height || 0}
+          className="absolute pointer-events-none -z-50 top-0 left-0 w-full h-full object-cover brightness-65"
+        />
+      )}
     </SectionGridFullPage>
   )
 }
