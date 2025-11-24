2fead026466b92e53e860b81605de3477870c230
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-11-25 00:02:11 +0100

    feat(about-skills): Add skill section to about page

--- apps/folio-2025-v1/src/components/typograpgy/display.tsx
+++ apps/folio-2025-v1/src/components/typograpgy/display.tsx
@@ -1,16 +1,19 @@
 import { cn } from '@/lib/utils/cn'
+import { forwardRef } from 'react'
 
-export function TDisplay({
-  children,
-  className,
-  ...props
-}: React.PropsWithChildren<React.HTMLAttributes<HTMLHeadingElement>>) {
+export const TDisplay = forwardRef<
+  HTMLHeadingElement,
+  React.PropsWithChildren<React.HTMLAttributes<HTMLHeadingElement>>
+>(({ children, className, ...props }, ref) => {
   return (
     <h1
+      ref={ref}
       className={cn('font-thin text-9xl h-fit font-made-mirage leading-[normal]', className)}
       {...props}
     >
       {children}
     </h1>
   )
-}
+})
+
+TDisplay.displayName = 'TDisplay'
